582bba68273dd6e130e7217ea678961f4a317e9b
Cam McKasson <cam@mckassons.com>
2016-04-23 16:00:14 -0700

    "finished" player locomotion

--- Assets/_Scripts/Player/PlayerWeaponController.cs
+++ Assets/_Scripts/Player/PlayerWeaponController.cs
@@ -1,15 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerWeaponController : MonoBehaviour {
+[RequireComponent(typeof(Animator))]
+public class PlayerWeaponController : MonoBehaviour
+{
+    public GameObject m_weaponHolder;
+
+    //DEBUG
+    public Transform leftHandHold, rightHandHold;
+
+    private Animator m_playerAnimator;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        m_playerAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
+
+    void OnAnimatorIK()
+    {
+        m_playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+        m_playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+        m_playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandHold.position);
+        m_playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandHold.rotation);
+
+        m_playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+        m_playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+        m_playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, rightHandHold.position);
+        m_playerAnimator.SetIKRotation(AvatarIKGoal.RightHand, rightHandHold.rotation);
+    }
 }
